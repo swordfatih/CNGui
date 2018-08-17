7e8d252751cc6ca1d9798747c7ab119a7128bda4
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-08-17 23:56:39 +0200

    Update

--- include/CNGui/Objects/Button.hpp
+++ include/CNGui/Objects/Button.hpp
@@ -1,65 +1,119 @@
-#ifndef BUTTON_HPP
-#define BUTTON_HPP
+/////////////////////////////////////////////////////////////////////////////////
+//
+// CNGui - Chats Noirs Graphical User Interface
+// Copyright (c) 2018 Fatih (accfldekur@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+/////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
-#include "CNGui/Objects/Object.hpp"
-#include "CNGui/Utilities/Shape.hpp"
-#include <SFML/Graphics/Text.hpp>
+#include "CNGui/Objects/Button.hpp"
 
 namespace CNGui
 {
 ////////////////////////////////////////////////////////////
-/// \brief Class that creates a graphical button
-///
-////////////////////////////////////////////////////////////
-class Button : public Object
+bool Button::onClick()
 {
-public:
-    using Object::Object;
-
-    ////////////////////////////////////////////////////////////
-    /// \brief Click event
-    ///
-    /// \return Returns true on button click
-    ///
-    ////////////////////////////////////////////////////////////
-    bool onClick();
-
-    ////////////////////////////////////////////////////////////
-    /// \brief Hover event
-    ///
-    /// \return Returns true when button hovered
-    ///
-    ////////////////////////////////////////////////////////////
-    bool onHover();
-
-private:
-    ////////////////////////////////////////////////////////////
-    /// \brief Updates the object
-    ///
-    ////////////////////////////////////////////////////////////
-    void update();
-
-    ////////////////////////////////////////////////////////////
-    /// \brief Draw the object to a render target
-    ///
-    /// \param target Render target to draw to
-    /// \param states Current render states
-    ///
-    ////////////////////////////////////////////////////////////
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-
-    ////////////////////////////////////////////////////////////
-    // Member data
-    ////////////////////////////////////////////////////////////
-    Shape       mShape;             ///< Shape of the button
-    sf::Text    mLabel;             ///< Label of the button
-    bool        mClicked = false;   ///< Button clicked
-    bool        mHover = false;     ///< Hovering the button
-};
-
+    return mClicked;
 }
 
-#endif // BUTTON_HPP
+////////////////////////////////////////////////////////////
+bool Button::onHover()
+{
+    return mHover;
+}
+
+////////////////////////////////////////////////////////////
+void Button::update()
+{
+    //Updating the style of the button
+    if(mUpdate)
+    {
+        mShape.setType(mStyle.shape);
+        mShape.setSize(mSize);
+        mShape.setFillColor(mStyle.fillcolor);
+        if(mStyle.outline)
+        {
+            mShape.setOutlineColor(mStyle.outlinecolor);
+            mShape.setOutlineThickness(mStyle.outlinethickness);
+        }
+        mLabel.setFont(mStyle.font);
+        mLabel.setFillColor(mStyle.labelcolor);
+        mLabel.setCharacterSize(mStyle.charactersize);
+        mLabel.setString(mName);
+        while(mLabel.getGlobalBounds().width > mShape.getGlobalBounds().width || mLabel.getGlobalBounds().height > mShape.getGlobalBounds().height)
+            mLabel.setCharacterSize(mLabel.getCharacterSize() - 1);
+        mLabel.setPosition(mShape.getGlobalBounds().width / 2 - mLabel.getGlobalBounds().width / 1.95, mShape.getGlobalBounds().height / 2 - mLabel.getGlobalBounds().height);
+        mUpdate = false;
+    }
+
+    //If the mouse hovers the button
+    if(sf::FloatRect(getPosition().x, getPosition().y, mShape.getGlobalBounds().width, mShape.getGlobalBounds().height).contains(mMouse))
+    {
+        mHover = true;
+
+        //If the mouse click on the boutton we throw an event by a bool
+        if(mHandleEvent.isActive(sf::Event::MouseButtonPressed) && mHandleEvent[sf::Event::MouseButtonPressed].mouseButton.button == sf::Mouse::Left)
+            mClicked = true;
+        else
+            mClicked = false;
+
+        //If mouse pressed
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            mShape.setFillColor(mStyle.clickedcolor);
+            mLabel.setFillColor(mStyle.labelclickedcolor);
+            mShape.setOutlineColor(mStyle.outlineclickedcolor);
+            mReturn = true;
+        }
+        //If mouse only hovers
+        else if(!mStyle.selectable || (mStyle.selectable && !mReturn))
+        {
+            mShape.setFillColor(mStyle.hovercolor);
+            mLabel.setFillColor(mStyle.labelhovercolor);
+            mShape.setOutlineColor(mStyle.outlinehovercolor);
+        }
+    }
+    //If mouse doesn't hover the button
+    else
+    {
+        mHover = false;
+
+        //If mouse isn't selected
+        if(!mReturn || (mReturn && mHandleEvent.isActive(sf::Event::MouseButtonPressed) && mHandleEvent[sf::Event::MouseButtonPressed].mouseButton.button == sf::Mouse::Left))
+        {
+            mShape.setFillColor(mStyle.fillcolor);
+            mLabel.setFillColor(mStyle.labelcolor);
+            mShape.setOutlineColor(mStyle.outlinecolor);
+            if(mReturn)
+                mReturn = false;
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////
+void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+
+    target.draw(mShape, states);
+    target.draw(mLabel, states);
+}
+
+} // namespace CNGui
