a39dc712641232b67d43287268a47e50b7808d0a
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-09-16 14:59:18 +0200

    Updated example because the recent changes

--- examples/main.cpp
+++ examples/main.cpp
@@ -15,8 +15,14 @@ int main()
     style.selectable = false;
     style.outline = true;
 
+    CNGui::Container<CNGui::Object> container(sf::Vector2f(500, 200), CNGui::Container<CNGui::Object>::Horizontal);
+
     CNGui::Button button("Button#001", sf::Vector3f(200, 100, 25), handleEvent, style);
-    button.setPosition(50, 50);
+    CNGui::Button button2("Start#002", sf::Vector3f(200, 100, 25), handleEvent, style);
+
+    container << button << button2;
+    container.setPosition(200, 100);
+    container.setSpacing(10);
 
     while(window.isOpen())
     {
@@ -31,11 +37,15 @@ int main()
             handleEvent.push(event);
         }
 
-        if(button(sf::Mouse::getPosition(window)))
+        if(button(window) && button.onClick())
             button << "I think thats too long hahahahha";
 
+        button2(window.mapPixelToCoords(sf::Mouse::getPosition(window), window.getDefaultView()));
+
         window.clear();
-        window.draw(button);
+        window.draw(container);
         window.display();
     }
+
+    return 0;
 }
