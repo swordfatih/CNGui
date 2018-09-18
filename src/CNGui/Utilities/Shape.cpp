779aa16a1e22861f5698cc2e65616558c9011b2c
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-09-18 19:31:13 +0200

    Removed radius, updated calculations for shapes

--- src/CNGui/Utilities/Shape.cpp
+++ src/CNGui/Utilities/Shape.cpp
@@ -1,9 +1,34 @@
+/////////////////////////////////////////////////////////////////////////////////
+//
+// CNGui - Chats Noirs Graphical User Interface
+// Copyright (c) 2018 Fatih (accfldekur@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
 #include "CNGui/Utilities/Shape.hpp"
 
 namespace CNGui
 {
 ////////////////////////////////////////////////////////////
-Shape::Shape(Type type, sf::Vector3f size) : mType(type), mSize(size)
+Shape::Shape(Type type, sf::Vector2f size) : mType(type), mSize(size)
 {
     update();
 }
@@ -28,14 +53,14 @@ Shape::Type Shape::getType()
 }
 
 ////////////////////////////////////////////////////////////
-void Shape::setSize(sf::Vector3f size)
+void Shape::setSize(sf::Vector2f size)
 {
     mSize = size;
     update();
 }
 
 ////////////////////////////////////////////////////////////
-sf::Vector3f Shape::getSize()
+sf::Vector2f Shape::getSize()
 {
     return mSize;
 }
@@ -48,7 +73,16 @@ std::size_t Shape::getPointCount() const
         default:
         case Triangle: return 3;
         case Rectangle: return 4;
-        case RoundedRectangle: return mSize.z * 4;
+        case RoundedRectangle:
+        {
+            float radius = 0;
+            if(mSize.x < mSize.y)
+                radius = mSize.x / 5;
+            else
+                radius = mSize.y / 5;
+            return radius * 4;
+        }
+
         case Circle: return 30;
     }
 }
@@ -64,10 +98,10 @@ sf::Vector2f Shape::getPoint(std::size_t index) const
             static const float pi = 3.141592654f;
 
             float angle = index * 2 * pi / 3 - pi / 2;
-            float x = std::cos(angle) * mSize.z;
-            float y = std::sin(angle) * mSize.z;
+            float x = std::cos(angle) * mSize.x;
+            float y = std::sin(angle) * mSize.y;
 
-            return sf::Vector2f(mSize.z + x, mSize.z + y);
+            return sf::Vector2f(mSize.x + x, mSize.y + y);
         }
         case Rectangle:
         {
@@ -82,30 +116,55 @@ sf::Vector2f Shape::getPoint(std::size_t index) const
         }
         case RoundedRectangle:
         {
-            float deltaAngle = 90.0f/(mSize.z-1);
+            float radius = 0;
+            if(mSize.x < mSize.y)
+                radius = mSize.x / 5;
+            else
+                radius = mSize.y / 5;
+
+            float deltaAngle = 90.0f/(radius-1);
             sf::Vector2f center;
-            unsigned int centerIndex = index/mSize.z;
+            unsigned int centerIndex = index/radius;
             static const float pi = 3.141592654f;
 
             switch(centerIndex)
             {
-                case 0: center.x = mSize.x - mSize.z; center.y = mSize.z; break;
-                case 1: center.x = mSize.z; center.y = mSize.z; break;
-                case 2: center.x = mSize.z; center.y = mSize.y - mSize.z; break;
-                case 3: center.x = mSize.x - mSize.z; center.y = mSize.y - mSize.z; break;
+                case 0:
+                    center.x = mSize.x - radius;
+                    center.y = radius;
+                    break;
+                case 1:
+                    center.x = radius;
+                    center.y = radius;
+                    break;
+                case 2:
+                    center.x = radius;
+                    center.y = mSize.y - radius;
+                    break;
+                case 3:
+                    center.x = mSize.x - radius;
+                    center.y = mSize.y - radius;
+                    break;
             }
 
-            return sf::Vector2f(mSize.z*cos(deltaAngle*(index-centerIndex)*pi/180)+center.x,-mSize.z*sin(deltaAngle*(index-centerIndex)*pi/180)+center.y);
+            return sf::Vector2f(radius*std::cos(deltaAngle*(index-centerIndex)*pi/180)+center.x,-radius*std::sin(deltaAngle*(index-centerIndex)*pi/180)+center.y);
         }
         case Circle:
         {
             static const float pi = 3.141592654f;
 
-            float angle = index * 2 * pi / 30 - pi / 2;
-            float x = std::cos(angle) * mSize.z;
-            float y = std::sin(angle) * mSize.z;
+            int radius = 0;
 
-            return sf::Vector2f(mSize.z + x, mSize.z + y);
+            if(mSize.x < mSize.y)
+                radius = mSize.x / 2;
+            else
+                radius = mSize.y / 2;
+
+            float angle = index * 2 * pi / 30 - pi / 2;
+            float x = std::cos(angle) * radius;
+            float y = std::sin(angle) * radius;
+
+            return sf::Vector2f(radius + x, radius + y);
         }
     }
 }
