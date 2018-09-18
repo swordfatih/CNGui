6640e9f5ec684073fc6fa96a41de67e8bb639600
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-09-18 19:16:53 +0200

    Update container-stacking.cpp

--- examples/container-stacking.cpp
+++ examples/container-stacking.cpp
@@ -3,32 +3,32 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "CNGui Example");
+    sf::RenderWindow window({640, 480}, "CNGui Example");
     window.setFramerateLimit(60);
 
     CNGui::EventHandler handleEvent;
 
     CNGui::Style style;
     style.shape = CNGui::Shape::RoundedRectangle;
-    style.fillcolor = sf::Color(200, 200, 200);
-    style.hovercolor = sf::Color(100, 100, 100);
+    style.fillcolor = {200, 200, 200};
+    style.hovercolor = {100, 100, 100};
     style.selectable = true;
     style.outline = true;
 
-    CNGui::Container<CNGui::Container<CNGui::Object>> containerMain(sf::Vector2f(500, 300), CNGui::ContainerType::Vertical);
+    CNGui::Button buttonStart("Start#001", handleEvent, style, {200, 100});
+    CNGui::Button buttonReset("Reset#001", handleEvent, style, {200, 100});
+    CNGui::Button buttonTestOne("Test#001", handleEvent, style, {200, 100});
+    CNGui::Button buttonTestTwo("Test#002", handleEvent, style, {200, 100});
 
-    CNGui::Container<CNGui::Object> containerButton(sf::Vector2f(200, 150), CNGui::ContainerType::Vertical);
-    CNGui::Container<CNGui::Object> containerTest(sf::Vector2f(200, 150), CNGui::ContainerType::Horizontal);
-
-    CNGui::Button buttonStart("Start#001", sf::Vector3f(200, 100, 25), handleEvent, style);
-    CNGui::Button buttonReset("Reset#001", sf::Vector3f(200, 100, 25), handleEvent, style);
-    CNGui::Button buttonTestOne("Test#001", sf::Vector3f(200, 100, 25), handleEvent, style);
-    CNGui::Button buttonTestTwo("Test#002", sf::Vector3f(200, 100, 25), handleEvent, style);
-
+    CNGui::Container<CNGui::Object> containerButton(CNGui::Vertical, sf::Vector2f(200, 150));
     containerButton << buttonStart << buttonReset;
+    
+    CNGui::Container<CNGui::Object> containerTest(CNGui::Horizontal, sf::Vector2f(200, 150));
     containerTest << buttonTestOne << buttonTestTwo;
 
+    CNGui::Container<CNGui::Container<CNGui::Object>> containerMain(CNGui::Vertical, {500, 300});
     containerMain << containerButton << containerTest;
+    
     containerMain.setSpacing(50);
     containerMain.setPosition(70, 50);
 
@@ -46,7 +46,7 @@ int main()
         }
 
         if(buttonStart(window) && buttonStart.onClick())
-            buttonStart << "I think thats too long hahahahha";
+            buttonStart << "I think thats too long";
 
         if(buttonReset(window.mapPixelToCoords(sf::Mouse::getPosition(window), window.getDefaultView())))
             buttonStart << "Start";
