5d54b0167326fd49b9365e0802e5dc3328c439bb
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-09-18 19:21:38 +0200

    Reordered constructor and fixed issue

--- include/CNGui/Containers/Container.hpp
+++ include/CNGui/Containers/Container.hpp
@@ -62,7 +62,7 @@ public:
     /// \brief Default constructor
     ///
     ////////////////////////////////////////////////////////////
-                    Container(const sf::Vector2f& size = sf::Vector2f(400, 400), ContainerType type = ContainerType::Free) : mSize(size), mType(type), mSpacing(10), mContainer(new sf::Vector2f(0, 0)), mDeep(0, 0)
+                    Container(ContainerType type = ContainerType::Free, const sf::Vector2f& size = sf::Vector2f(400, 400)) : mSize(size), mType(type), mSpacing(15), mContainer(new sf::Vector2f(0, 0)), mDeep(0, 0)
     {
         static_assert(std::is_base_of<sf::Drawable, Content>::value && std::is_base_of<sf::Transformable, Content>::value, "Invalid type, must be Drawable and Transformable");
     }
@@ -225,27 +225,12 @@ protected:
 
             if(mType == ContainerType::Horizontal)
             {
-                if(std::is_base_of<CNGui::Object, Content>::value)
-                {
-                    mContents[i].get().setSize(sf::Vector3f(mSize.x / mContents.size(), mSize.y, mContents[i].get().getSize().z));
-                }
-                else
-                {
-                    mContents[i].get().setSize(sf::Vector2f(mSize.x / mContents.size(), mSize.y));
-                }
-
+                mContents[i].get().setSize(sf::Vector2f(mSize.x / mContents.size(), mSize.y));
                 mContents[i].get().setPosition(i * (mContents[i].get().getSize().x + mSpacing), 0);
             }
             else if(mType == ContainerType::Vertical)
             {
-                if(std::is_base_of<CNGui::Object, Content>::value)
-                {
-                    mContents[i].get().setSize(sf::Vector3f(mSize.x, mSize.y / mContents.size(), mContents[i].get().getSize().z));
-                }
-                else
-                {
-                    mContents[i].get().setSize(sf::Vector2f(mSize.x, mSize.y / mContents.size()));
-                }
+                mContents[i].get().setSize(sf::Vector2f(mSize.x, mSize.y / mContents.size()));
                 mContents[i].get().setPosition(0, i * (mContents[i].get().getSize().y + mSpacing));
             }
         }
