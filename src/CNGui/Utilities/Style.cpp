d73bf89910d71e8213bd50bf20f0d1b9310e0f8f
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-09-23 18:55:46 +0200

    New styles

--- src/CNGui/Utilities/Style.cpp
+++ src/CNGui/Utilities/Style.cpp
@@ -31,23 +31,24 @@ namespace CNGui
 Style::Style() :
     shape(Shape::Rectangle),
     charactersize(24),
-    fillcolor(sf::Color(249, 159, 93)),
-    hovercolor(sf::Color(220, 140, 82)),
-    clickedcolor(sf::Color(230, 130, 60)),
-    labelcolor(sf::Color(255, 255, 255)),
-    labelhovercolor(sf::Color(220, 220, 220)),
-    labelclickedcolor(sf::Color(200, 100, 30)),
+    fillcolor(200, 200, 200),
+    hovercolor(100, 100, 100),
+    clickedcolor(230, 130, 60),
+    labelcolor(255, 255, 255),
+    labelhovercolor(220, 220, 220),
+    labelclickedcolor(200, 100, 30),
     selectable(false),
     outline(false),
     outlinethickness(3),
-    outlinecolor(sf::Color(150, 150, 150)),
-    outlinehovercolor(sf::Color(50, 50, 50)),
-    outlineclickedcolor(sf::Color(150, 80, 20)),
+    outlinecolor(150, 150, 150),
+    outlinehovercolor(50, 50, 50),
+    outlineclickedcolor(150, 80, 20),
     backgroundcolor(sf::Color::Transparent),
-    backgroundhovercolor(sf::Color(220, 220, 220)),
-    backgroundclickedcolor(sf::Color(155, 155, 155)),
+    backgroundhovercolor(220, 220, 220),
+    backgroundclickedcolor(155, 155, 155),
     animated(true),
-    label(true)
+    label(true),
+    durationanimation(sf::seconds(3))
 {
     font.loadFromMemory(Font::BebasNeue::hex, Font::BebasNeue::size);
 }
