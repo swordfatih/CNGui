abc76841e6ba2c97db039c59375817e5ab97bb1f
swordfatih <35528737+swordfatih@users.noreply.github.com>
2019-06-23 16:11:32 +0200

    Switched Container.hpp directory

--- include/CNGui/CNGui.hpp
+++ include/CNGui/CNGui.hpp
@@ -1,28 +1,3 @@
-/////////////////////////////////////////////////////////////////////////////////
-//
-// CNGui 1.1 - Chats Noirs Graphical User Interface
-// Copyright (c) 2019 Fatih (accfldekur@gmail.com)
-//
-// This software is provided 'as-is', without any express or implied
-// warranty. In no event will the authors be held liable for any damages
-// arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented; you must not
-//    claim that you wrote the original software. If you use this software
-//    in a product, an acknowledgment in the product documentation would be
-//    appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such, and must not be
-//    misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source distribution.
-//
-/////////////////////////////////////////////////////////////////////////////////
-
 #ifndef CNGUI_HPP_INCLUDED
 #define CNGUI_HPP_INCLUDED
 
@@ -35,15 +10,13 @@
 #include <CNGui/Core/EventHandler.hpp>
 #include <CNGui/Core/Updatable.hpp>
 #include <CNGui/Core/Style.hpp>
+#include <CNGui/Core/Container.hpp>
 
 //Utilities
 #include <CNGui/Tools/Font.hpp>
 #include <CNGui/Tools/FunctionWrapper.hpp>
 #include <CNGui/Tools/Shape.hpp>
 #include <CNGui/Tools/Text.hpp>
-
-//Containers
-#include <CNGui/Containers/Container.hpp>
 
 //Objects
 #include <CNGui/Objects/Object.hpp>
