877b9ab1d0a3f953325babedd81fe76b7f4c31a2
swordfatih <35528737+swordfatih@users.noreply.github.com>
2019-12-05 17:18:50 +0100

    updated getting-started

--- examples/getting-started/getting-started.cpp
+++ examples/getting-started/getting-started.cpp
@@ -21,17 +21,6 @@ int main()
     container_main.setPosition(200, 200);
     container_main << progression << container_buttons;
 
-    sf::Texture texture;
-    texture.loadFromFile("examples/getting-started/shop.png");
-
-    CNGui::Poster poster("News !", {}, {200, 200});
-    poster.setPosition(200, 600);
-    poster.getStyle().background_color.neutral = {100, 200, 255, 255};
-    poster.getStyle().background_texture = texture;
-    poster.getStyle().animation_duration = sf::seconds(2);
-    poster.setDate("07/08/2019");
-    poster.setDescription("Nothing to report today..");
-
     while(window.isOpen())
     {
         handleEvent.process(window);
@@ -52,8 +41,6 @@ int main()
         }
 
         progression();
-
-        poster(window);
 
         window.clear();
         CNGui::Core::draw(window);
